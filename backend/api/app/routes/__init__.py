1c7d620112b02d9993211b7f4ca7741cdbc0ef43
Josh Cao <joshcaopdx@gmail.com>
2026-02-28 19:56:37 -0800

    Sorry zach this is so cursed

--- backend/api/app/routes/__init__.py
+++ backend/api/app/routes/__init__.py
@@ -1,6 +1,6 @@
-from .audio_routes import audio_bp
-from .translate_file import translate_bp
-
 def register_routes(app):
+    from .audio_routes import audio_bp
+    from .translate_file import translate_bp
+
     app.register_blueprint(audio_bp, url_prefix='/api/audio')
     app.register_blueprint(translate_bp, url_prefix='/api/translate')

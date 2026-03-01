74ef1fbbf57b927a490681ae9764ef0ac8432004
zmalinoski <zmalinoski@scu.edu>
2026-02-28 20:44:19 -0800

    change url to deployed

--- spatial-translate/src/hooks/listener.js
+++ spatial-translate/src/hooks/listener.js
@@ -1,7 +1,7 @@
 import { useState, useEffect, useRef } from 'react';
 import { io } from 'socket.io-client';
 
-const socket = io('http://localhost:5001');
+const socket = io('https://spatial-translate-11.onrender.com/api/audio/');
 
 export function useSpeechRecognition() {
   const [history, setHistory] = useState([]);
